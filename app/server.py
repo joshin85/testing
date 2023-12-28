2d5788414aaa566268f3e4a3a7498136f17e686c
Shinjo Melosh <joshin85@gmail.com>
2023-12-27 20:56:54 -0800

    asdfasdfasd
    asdfasdfasdfasdf

--- app/server.py
+++ app/server.py
@@ -17,10 +17,6 @@ async def redirect_root_to_docs():
 async def redirect_root_to_docs():
     return RedirectResponse("/docs")
 
-@app.get("/aasdfasdfsdf")
-async def redirect_root_to_docs():
-    return RedirectResponse("/docs")
-
 if __name__ == "__main__":
     import uvicorn
 
