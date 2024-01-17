3152ab8d5c6842349a91122b3f114ffb2143fb3f
Shinjo Melosh <joshin85@gmail.com>
2024-01-16 20:01:22 -0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,6 +9,7 @@ app = FastAPI()
 async def redirect_root_to_docs():
     return RedirectResponse("/docs")
 
+
 @app.get("/asdf")
 async def redirect_root_to_docs():
     return RedirectResponse("/docs")
